4791a761c762e71a729d9a24beb020112f56b3ad
Masadow <jimmy.delas@gmail.com>
2013-07-03 10:09:20 +0200

    Minor change to improve setup

--- config_list.php
+++ config_list.php
@@ -10,5 +10,7 @@ if ($handle = opendir('config')) {
     closedir($handle);
 }
 
-$current_id = 1;
+$current_id = 2;
+if (!isset($conf_list[$current_id]))
+	$current_id = 0;
 $current = $conf_list[$current_id];
